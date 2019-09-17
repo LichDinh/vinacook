b19dc0d587b7ac6384c9e34ff40809f0a065e6b4
lich <lich.dinh@tomosia.com>
2019-09-17 18:14:25 +0700

    add seed

--- db/migrate/20190313125048_create_orders.rb
+++ db/migrate/20190313125048_create_orders.rb
@@ -3,7 +3,7 @@ class CreateOrders < ActiveRecord::Migration[5.1]
     create_table :orders do |t|
       t.integer :subtotal
       t.integer :code
-      t.integer :phone
+      t.string :phone
       t.string :email
       t.string :name
       t.string :address
